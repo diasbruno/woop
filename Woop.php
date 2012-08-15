4e4216c3ab0a298b3b58b00b04e858e73b1421ae
Bruno Dias <contact@diasbruno.com.br>
2012-08-15 03:06:50 -0300

    update first classes.

---
+++ Woop.php
@@ -0,0 +1,10 @@
+<?php  
+
+/**
+ * This file includes all other files from WooP.
+ */
+include_once( 'WoopIterator.php' );
+include_once( 'WoopTag.php' );
+include_once( 'WoopBookmark.php' );
+
+?>
